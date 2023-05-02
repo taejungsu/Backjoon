adcf1aff62013bd2834fe10b4dacdde42c41fff8
taejungsu <102867460+taejungsu@users.noreply.github.com>
2023-05-02 13:54:09 +0900

    [Bronze III] Title: 공 넣기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10810. 공 넣기/공 넣기.py
@@ -0,0 +1,11 @@
+
+n,m = map(int, input().split())
+ball = [0]*n
+
+for _ in range(m):
+  i,j,k = map(int, input().split())
+  for x in range(i, j+1):
+    ball[x-1] = k
+
+for x in range(n):
+  print(ball[x] , end=' ')
