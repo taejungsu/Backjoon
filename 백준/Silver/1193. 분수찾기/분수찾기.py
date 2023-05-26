be114e3762a801d0988dedc73daddcdead54b0a9
태정수 <102867460+taejungsu@users.noreply.github.com>
2023-05-26 09:43:26 +0900

    [Silver V] Title: 분수찾기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1193. 분수찾기/분수찾기.py
@@ -0,0 +1,15 @@
+n = int(input())
+line = 1
+
+while n > line :
+    n -= line
+    line += 1
+
+if line % 2 == 0 :
+    num1 = n
+    num2 = line - n + 1
+elif line % 2 == 1 :
+    num1 = line - n + 1
+    num2 = n
+
+print(num1,'/',num2,sep = "")
