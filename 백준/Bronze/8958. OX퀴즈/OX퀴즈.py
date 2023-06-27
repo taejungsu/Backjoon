26a2164262eed85ed6ef64110b6b3a9273ba5e9f
태정수 <102867460+taejungsu@users.noreply.github.com>
2023-06-27 14:17:45 +0900

    [Bronze II] Title: OX퀴즈, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.py
@@ -0,0 +1,13 @@
+N = int(input())
+
+for i in range(N):
+    a = input()
+    score = 0
+    sumScore = 0
+    for j in a:
+        if j == 'O':
+            score += 1
+        else:
+            score = 0
+        sumScore += score
+    print(sumScore)
