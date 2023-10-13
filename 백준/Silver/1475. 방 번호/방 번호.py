86843c7cadedb0a1726d459b61d05efe0092df11
태정수 <xowjdtn@naver.com>
2023-10-13 10:18:08 +0900

    [Silver V] Title: 방 번호, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1475. 방 번호/방 번호.py
@@ -0,0 +1,12 @@
+n = int(input())
+card = [0] * 10
+for i in str(n):
+    if i == "9" or i == "6":
+        if card[6] == card[9]:
+            card[6] += 1
+        else:
+            card[9] += 1
+    else:
+        card[int(i)] += 1
+
+print(max(card))
