a47f25b5511071dbcec4ecc2cb84f52c392b73df
taejungsu <xowjdtn@naver.com>
2023-10-19 09:29:36 +0900

    [Silver IV] Title: 듣보잡, Time: 5696 ms, Memory: 43924 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -0,0 +1,14 @@
+N , M = map(int,input().split())
+arr_1 = set()
+arr_2 = set()
+
+for _ in range(N):
+    arr_1.add(input())
+for _ in range(M):
+    arr_2.add(input())
+
+arr = sorted(list(arr_1 & arr_2))
+print(len(arr))
+
+for i in arr:
+    print(i)
