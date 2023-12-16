fbb15ef26de66a33ff52a1d258c0c9c38496b7af
taejungsu <xowjdtn@naver.com>
2023-12-16 21:23:25 +0900

    [Silver V] Title: 부분 문자열, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/6550. 부분 문자열/부분 문자열.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+while True:
+    try:
+        s,t = map(str,input().split())
+        first = list(s)
+        for i in t:
+            if first and i == first[0]:
+                first.pop(0)
+        if not first:
+            print("Yes")
+        else:
+            print("No")
+    except:
+        break
